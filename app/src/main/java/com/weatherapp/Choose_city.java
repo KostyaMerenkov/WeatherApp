fb7210a8acf9b9df189177f08061da0b204fd649
Kostya <KonstantinLikes@gmail.com>
2020-11-02 21:49:35 +1000

    Commit

--- app/src/main/java/com/weatherapp/Choose_city.java
+++ app/src/main/java/com/weatherapp/Choose_city.java
@@ -1,9 +1,16 @@
 package com.weatherapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -17,22 +24,66 @@ import java.util.List;
 public class Choose_city extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "CITY";
+    public final static String TAG = "CHOOSE_CITY";
+    private final static boolean DEBUG = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_city);
-
-
         AutoCompleteTextView editText = findViewById(R.id.actv);
-
-
+        editText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                editText.showDropDown();
+                editText.requestFocus();
+                return false;
+            }
+        });
         String[] cities = getResources().getStringArray(R.array.cities);
-
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, cities);
         editText.setAdapter(adapter);
+        if (DEBUG) {
+            Toast.makeText(getApplicationContext(), "onCreate()", Toast.LENGTH_SHORT).show();
+            detectOrientation();
+        }
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (DEBUG) {
+            Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "onPause()");
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if (DEBUG) {
+            Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "onRestart()");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (DEBUG) {
+            Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "onResume()");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (DEBUG) {
+            Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "onDestroy()");
+        }
     }
 
     public void weather(View view) {
@@ -45,7 +96,21 @@ public class Choose_city extends AppCompatActivity {
         String message = editText.getText().toString();
         // Добавляем с помощью свойства putExtra объект - первый параметр - ключ,
         // второй параметр - значение этого объекта
+        intent.putExtra(EXTRA_MESSAGE, message);
         // запуск activity
         startActivity(intent);
+    }
+
+    public void detectOrientation() {
+        Context appContext = getApplicationContext();
+        Configuration configuration = getResources().getConfiguration();
+        if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Toast.makeText(appContext, "Портретная ориентация", Toast.LENGTH_LONG).show();
+            Log.d(TAG, "Портретная ориентация");
+        }
+        if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(appContext, "Альбомная ориентация", Toast.LENGTH_LONG).show();
+            Log.d(TAG, "Альбомная ориентация");
+        }
     }
 }
--- app/src/main/java/com/weatherapp/MainActivity.java
+++ app/src/main/java/com/weatherapp/MainActivity.java
@@ -1,19 +1,100 @@
 package com.weatherapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final boolean DEBUG = true;
+    public final static String TAG = "MainApp";
+    private int temperature = 16;
+    private TextView tempTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         Intent intent = getIntent();
         String message = intent.getStringExtra(Choose_city.EXTRA_MESSAGE);
-        //TODO: передать название города
-        setContentView(R.layout.activity_main);
+        TextView city = (TextView) findViewById(R.id.textView2);
+        city.setText(message);
+        tempTextView = findViewById(R.id.textView2);
+        if (DEBUG) {
+            Toast.makeText(getApplicationContext(), "onCreate()", Toast.LENGTH_SHORT).show();
+            detectOrientation();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (DEBUG) {
+            Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "onPause()");
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if (DEBUG) {
+            Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "onRestart()");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (DEBUG) {
+            Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "onResume()");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (DEBUG) {
+            Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "onDestroy()");
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putInt("Temp", temperature);
+    }
+
+    @Override
+    public void onRestoreInstanceState(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState);
+        temperature = savedInstanceState.getInt("Temp");
+        tempTextView.setText(((Integer)temperature).toString());
+    }
+
+    public void detectOrientation() {
+        Context appContext = getApplicationContext();
+        Configuration configuration = getResources().getConfiguration();
+        if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Toast.makeText(appContext, "Портретная ориентация", Toast.LENGTH_LONG).show();
+            Log.d(TAG, "Портретная ориентация");
+        }
+        if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(appContext, "Альбомная ориентация", Toast.LENGTH_LONG).show();
+            Log.d(TAG, "Альбомная ориентация");
+        }
     }
 }
